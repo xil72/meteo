0645a784d45f3643bb8ba210a9ace0b34e20dd23
xil72 <glorialuna1555@gmail.com>
2025-03-17 13:33:28 -0400

    Injected more weather data

--- index.js
+++ index.js
@@ -2,11 +2,41 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+   
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = '${response.data.temperature.humidity}%';
+    windSpeedElement.innerHTML = '${response.data.wind.speed}km/h';
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday", 
+    "Monday",
+    "Tuesday",
+    "Wednesday", 
+    "Thursday",  
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+   
+  if (minutes < 10) {
+    minutes = '0${minutes}';
+  }
+  return '${day} ${hours}:${minutes}';
+}
 
 function searchCity(city) {
   let apiKey = "014202fo4a0e6360498b0cc710dbd1t4";
@@ -17,8 +47,8 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+ 
+  
   searchCity(searchInput.value);
 }
 
